203d87f96e0ee5e08ff2ca2ac2e0247aa404a118
Carlos Armando Plata Hernández <carlos.plata@gmail.com>
2023-12-05 12:51:55 -0600

    Added gitignore and Reincorporated first practice

--- promedio/promedio.c
+++ promedio/promedio.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
 int main(){
-    int cal1, cal2, cal3;
-    float res;
-    printf("Bienvenido, este programa sacará tu promedio final y te dirá si aprobaste ó reprobaste el semestre \n");
-    printf("A continuación, introduce tus tres calificaciones parciales, usa números enteros en dígitos y separa cada calificación con un espacio.\n \n");
-    scanf("%d %d %d", &cal1, &cal2, &cal3);
-    res = (cal1 + cal2 + cal3) / 3.0;
-    printf("Tu calificación es %2.1f \n", res); 
-    if (res >= 7.0){
-        printf("Felicidades! Aprobaste el semestre! \n");
-    }else{
-        printf("Lo sentimos, esta calificación es reprobatoria. \n");
+  int cal1, cal2, cal3;
+  float res;
+  printf("Bienvenido, este programa sacará tu promedio final y te dirá si aprobaste ó reprobaste el semestre \n");
+  printf("A continuación, introduce tus tres calificaciones parciales, usa números enteros en dígitos y separa cada calificación con un espacio\n \n");
+  scanf("%d %d %d", &cal1, &cal2, &cal3);
+  res = (cal1 + cal2 + cal3) / 3.0;
+  printf("Tu calificación es %2.1f \n", res);
+  if (res >= 6.0){
+    printf("Fe1icidades! Aprobaste el semestre!");
+  }else{
+    printf("Lo sentimos, esta calificación es reprobatoria." );
     }
+  scanf("%d", &cal1);
 return 0;
 }
